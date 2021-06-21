d180972c9d1cf7834070be11fd19ed1ede9300e6
David Green <greensopinion@gmail.com>
2021-06-21 06:48:00 -0700

    connect example

--- example/lib/line_chart_content.dart
+++ example/lib/line_chart_content.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:simple_line_chart/simple_line_chart.dart';
 
 class LineChartContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('placeholder');
+    return LineChart();
   }
 }
--- lib/simple_line_chart.dart
+++ lib/simple_line_chart.dart
@@ -1,7 +1,3 @@
 library simple_line_chart;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/line_chart.dart';
